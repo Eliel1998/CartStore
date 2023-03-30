a07292a1d41c610d645ce502e809a83e1ca4c961
Eliel Oliver <eliel.viana@grupomateus.com>
2023-03-30 19:25:39 -0300

    first commit

---
+++ src/main/java/com/example/CartStore/CartStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.CartStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CartStore/CartStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CartStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
